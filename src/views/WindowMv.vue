864a21eac900a85dc2fb7f08523fb292431297ac
Leider17 <leidersolano133@gmail.com>
2025-11-23 14:48:36 -0500

    fix: update novnc route

--- src/views/WindowMv.vue
+++ src/views/WindowMv.vue
@@ -18,9 +18,9 @@
             return
         }
         document.title = route.query.name
-        const websocketUrl = route.params.websocketUrl
+        const token = route.params.websocketUrl
 
-        const rfb = new RFB(screen.value, websocketUrl,{
+        const rfb = new RFB(screen.value, `ws://192.99.37.28:8000/vnc-proxy/${token}`,{
             compressionLevel: 5,
             qualityLevel: 2,
             enableWebP: true
